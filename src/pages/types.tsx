d865fd10e3545f5b6d6aa852e7d86e50df2e33d3
darkMasterv <79690406+lightdarkmaster@users.noreply.github.com>
2025-04-03 21:27:16 +0800

    Remove Unused Types

--- src/pages/types.tsx
+++ src/pages/types.tsx
@@ -1,20 +1,4 @@
 
-export interface User {
-    username: string;
-    password: string;
-  }
-  
-  export interface Properties {
-    users: User[];
-    setUsers: React.Dispatch<React.SetStateAction<User[]>>;
-  }
-  
-
- export interface UserModalProps {
-    user: User | null;
-    onClose: () => void;
-  }
-
   export type Advice = {
     minScore: number;
     maxScore: number;
